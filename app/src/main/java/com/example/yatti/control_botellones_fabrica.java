902afbddc1c4c01e3a08a78d17658e9bf7cafc6e
Karla Antonia Jimenez Rios <kajimenez.attoismto@gmail.com>
2019-05-22 15:21:12 -0500

    Actualizando los nombres de los class java

---
+++ app/src/main/java/com/example/yatti/control_botellones_fabrica.java
@@ -0,0 +1,24 @@
+package com.example.yatti;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+public class control_botellones_fabrica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_medicion_fabrica);
+        setupActionBar();
+    }
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Botellones");
+
+        }
+    }
+}
