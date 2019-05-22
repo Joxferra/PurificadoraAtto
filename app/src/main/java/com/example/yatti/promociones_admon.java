f008b00ce689f2ce872c0f55b8fd1bda2c127769
Gabriel Santiago Vasquez <sgabriel.attoistmo@gmail.com.mx>
2019-05-22 17:04:22 -0500

    modificacion de nombres1

---
+++ app/src/main/java/com/example/yatti/promociones_admon.java
@@ -0,0 +1,13 @@
+package com.example.yatti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class promociones_admon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promociones_admon);
+    }
+}
