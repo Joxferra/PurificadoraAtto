9dc015611a10e02afc841ec5c154d5a370109d85
Karla Antonia Jimenez Rios <kajimenez.attoismto@gmail.com>
2019-05-22 17:25:27 -0500

    Actualizando los nombres de los class java

--- app/src/main/java/com/example/yatti/control_fabricacion_fabrica.java
+++ app/src/main/java/com/example/yatti/control_fabricacion_fabrica.java
@@ -1,6 +1,7 @@
 package com.example.yatti;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 public class control_fabricacion_fabrica  extends AppCompatActivity {
@@ -8,5 +9,14 @@ public class control_fabricacion_fabrica  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_fabricacion_fabrica);
+    }
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Control de fabricacion");
+
+        }
     }
 }
