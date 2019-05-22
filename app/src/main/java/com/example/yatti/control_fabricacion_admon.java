2497f38717a8182d740327e76b353328d1f4a84e
ivan hernandez rangel <hivan.attoistmo@gmail.com>
2019-05-22 14:21:08 -0500

    cambio de los nombres de los activitys

--- app/src/main/java/com/example/yatti/control_fabricacion_admon.java
+++ app/src/main/java/com/example/yatti/control_fabricacion_admon.java
@@ -3,11 +3,11 @@ package com.example.yatti;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class control_fabricacion extends AppCompatActivity {
+public class control_fabricacion_admon extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_control_fabricacion);
+        setContentView(R.layout.activity_control_fabricacion_admon);
     }
 }
