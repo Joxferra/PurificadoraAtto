57e32bb274bf4098d38d09ea80d608bf568b5337
Gabriel Santiago Vasquez <sgabriel.attoistmo@gmail.com.mx>
2019-05-23 11:01:56 -0500

    actualizacion1

---
+++ app/src/main/java/com/example/yatti/mantenimiento_admon.java
@@ -0,0 +1,26 @@
+package com.example.yatti;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mantenimiento_admon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mantenimiento_admon);
+        setupActionBar();
+    }
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Mantenimiento");
+
+        }
+    }
+}
+
+
