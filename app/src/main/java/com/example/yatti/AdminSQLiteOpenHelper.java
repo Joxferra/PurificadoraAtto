6fd40591fdd3ae2a9270952ddd40cb9f1fff68cb
ivan hernandez rangel <hivan.attoistmo@gmail.com>
2019-05-31 11:35:55 -0500

    creacion de tablas

--- app/src/main/java/com/example/yatti/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/yatti/AdminSQLiteOpenHelper.java
@@ -19,9 +19,13 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
 
         db.execSQL("CREATE TABLE alta_cliente_admon(idcliente INTEGER PRIMARY KEY AUTOINCREMENT,nombre text,fecha text,ciudad text,colonia text,calle text,Nmcasa int,telefono int,ubicacion text )");
         db.execSQL("CREATE TABLE alta_personal_admon(idpersonal INTEGET PRIMARY KEY AUTOINCREMENT, nombre text, fecha text,curp text, direccion text,telefono int,perfil text,observaciones text)");
-        db.execSQL("CREATE TABLE alta_control_entrada_admon(identrada INTEGER PRIMARY KEY AUTOINCREMENT, fecha text, cargo text, nombre text, hora_entrada text, hora_ruta text )");
-
-        
+        db.execSQL("CREATE TABLE control_entrada_admon(identrada INTEGER PRIMARY KEY AUTOINCREMENT, fecha text, cargo text, nombre text, hora_entrada text, hora_ruta text )");
+        db.execSQL("CREATE TABLE control_fabricacion_admon(idfabricacion INTEGER PRIMARY KEY AUTOINCREMENT, fecha text, hora text, nombre text,lavados int, llenados int, entregados int,recibidos int, observaciones text) ");
+        db.execSQL("CREATE TABLE cotrol_vendedor_geoposicion (idgeoposicion INTEGER PRIMARY KEY AUTOINCREMENT, nombre text, geoposicion text, kilometraje text, gasolina text )");
+        db.execSQL("CREATE TABLE corte_dia_admon(idcorte INTEGER PRIMARY KEY AUTOINCREMENT, nombre text, fecha text, vendida text,  venta text, credito text, sobrante  text)");
+        db.execSQL("CREATE TABLE inventario_admon (idinventario INTEGER PRIMARY KEY AUTOINCREMENT, nombre text, cantidad int, descripcion text, fecha text )");
+        db.execSQL("CREATE TABLE mantenimiento_admon(idmantenimiento INTEGER PRIMARY KEY AUTOINCREMENT, nombre text, descripcion text, fecha text)");
+        db.execSQL("CREATE TABLE promociones_admon(idpromociones INTEGER PRIMARY KEY AUTOINCREMENT, nombre text, fechaf text, fechai text, obsetvaciones text)");
     }
 
     @Override
